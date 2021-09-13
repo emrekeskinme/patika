0050958d62d659a108ab05692b9e295d263ae907
Emre Keskin <emrekeskin60@gmail.com>
2021-09-13 05:06:06 +0300

    Hata Yönetimi
    Hata Yönetimi

---
+++ C#/hata-yonetimi/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    Console.WriteLine("Bir Sayı Giriniz: ");
+            //    int sayi1 = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Sayı: " + sayi1);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Hata: " + ex.Message.ToString());
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("İşlem Tamamlandı!");
+            //}
+
+            try
+            {
+                //int a = int.Parse(null);
+                int a = int.Parse("test");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz!");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Yanlış veri tipi!");
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
